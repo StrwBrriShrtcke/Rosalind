5ce479d8442750458556e9f327c53b6aad2695b1
Jia Wei <leejiawei241@gmail.com>
2025-02-08 21:43:59 +0800

    Added more typescript stuff (✿ᴗ͈ˬᴗ͈)⁾⁾

--- revc/solution.ts
+++ revc/solution.ts
@@ -1,21 +1,11 @@
 import * as fs from "node:fs";
-const dnaString: string= fs.readFileSync("./dataset.txt", "utf8");
-const dnaStringArray:string[] = dnaString.split("")
-dnaStringArray.pop()
+const dnaString: string = fs.readFileSync("./sample.txt", "utf8").trim();
+const dnaStringArray: string[] = dnaString.split("")
 dnaStringArray.reverse();
 
-for (let i = 0; i < dnaStringArray.length; i++) {
-  if (dnaStringArray[i] === "A") {
-    dnaStringArray.splice(i, 1, "T")
-  } 
-  else if (dnaStringArray[i] === "T") {
-    dnaStringArray.splice(i, 1, "A")
-  } 
-  else if (dnaStringArray[i] === "C") {
-    dnaStringArray.splice(i, 1, "G")
-  } 
-  else if (dnaStringArray[i] === "G") {
-    dnaStringArray.splice(i, 1, "C")
-  } 
-}
-fs.writeFileSync("./answer.txt", dnaStringArray.join(""))
+type Complement = { [K in Nucleotide]: Nucleotide }
+type Nucleotide = "A" | "T" | "G" | "C"
+const complement: Complement = { A: "T", T: "A", G: "C", C: "G" }
+const answerArray = dnaStringArray.map((nucleotide: Nucleotide) => complement[nucleotide])
+const answer = answerArray.join("")
+fs.writeFileSync("./answer.txt", answer)
