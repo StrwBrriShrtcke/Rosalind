98f5a83d32d691e573d860da7c0446e4787535a8
Jia Wei <leejiawei241@gmail.com>
2025-02-17 01:54:24 +0800

    Optimised code ✧｡٩(ˊᗜˋ )و✧*｡

--- prot/solution.ts
+++ prot/solution.ts
@@ -1,27 +1,27 @@
 import * as fs from 'node:fs'
 const rnaString: string = fs.readFileSync('./dataset.txt', 'utf8');
 
-const rnaCodon = fs.readFileSync("./rnacodon.txt", "utf8")
+const rnaCodon: string = fs.readFileSync("./rnacodon.txt", "utf8")
 rnaCodon.trim();
 
-const rna = rnaCodon.
-replaceAll(/\w{3}\ \w{4}/g, "").
-replaceAll(/\s/g, "")
+const rna: string = rnaCodon.
+  replaceAll(/Stop/g, "s").
+  replaceAll(/\s/g, "")
 
 const codonTable: object = {}
 
 // Function to make the codon table from rnacodon.txt into an object
 function txtToObj(number: number) {
-  const start = number * 4
+  const start: number = number * 4
   return codonTable[rna.substring(start, start + 3)] = rna[start + 3]
 }
 
 // Iterate through all elements in codonTable
-for (let i = 0; i < rna.length; i++) {
+for (let i: number = 0; i < rna.length; i++) {
   txtToObj(i)
 }
 
-for (let i = 0; i < rnaCodon.length; i++) {
+for (let i: number = 0; i < rnaCodon.length; i++) {
   rnaCodon.trim().substring(0, 3)
 }
 
@@ -30,28 +30,26 @@ rnaString.trim()
 // Create array for RNA
 const rnaArray: string[] = []
 
-// Function to seperate string of letters into array of individual RNA sequences
+// Function to seperate rnaString into array of individual RNA sequences
 function rnaCode(start: number) {
   return rnaArray.push(rnaString.substring(start, start + 3))
 }
 
-// Iterate through rnaString
-for (let i = 0; i <= rnaString.length / 3 - 1; i++) {
+// Seperate rnaString into individual RNA sequences and transform these sequences into its corresponding amino acid
+for (let i: number = 0; i <= rnaString.length / 3 - 1; i++) {
   rnaCode(i * 3)
-}
-
-// Transforming rna sequence into coresponding amino acid
-for (let i = 0; i < rnaArray.length; i++) {
   if (codonTable
-  [rnaArray[i]] !== undefined) {
+  [rnaArray[i]] !== undefined && codonTable[rnaArray[i]] !== "s") {
     rnaArray[i] = codonTable
     [rnaArray[i]]
   }
-  else if (codonTable
-  [rnaArray[i]] === undefined) {
-    rnaArray[i] = "stop"
+  else {
+    rnaArray.splice(i, 1)
   }
+
 }
-// removing stop codons and transforming array into string
-const answer = rnaArray.filter((x) => x !== "stop").join("")
+
+// Transforming Array into String
+const answer = rnaArray.join("")
+console.log(answer)
 fs.writeFileSync("./answer.txt", answer)
