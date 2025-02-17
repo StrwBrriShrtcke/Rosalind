06f266435fb971191c4484d1f180040ece85be9f
Jia Wei <leejiawei241@gmail.com>
2025-02-17 19:35:25 +0800

    Changed the way I made codonTable such that it does not modify the original data ଘ(੭*ˊᵕˋ)੭* ੈ♡‧₊˚

--- prot/solution.ts
+++ prot/solution.ts
@@ -1,26 +1,24 @@
 import * as fs from 'node:fs'
 const rnaString: string = fs.readFileSync('./dataset.txt', 'utf8');
-
 const rnaCodon: string = fs.readFileSync("./rnacodon.txt", "utf8")
-const rna: string = rnaCodon
-  .replaceAll(/\w{3}\ \w{4}/g, "")
-  .replaceAll(/\s/g, "")
+// const rna: string = rnaCodon
+//   .replaceAll(/\w{3}\ \w{4}/g, "")
+//   .replaceAll(/\s/g, "")
 
-const codonTable: object = {}
 
 // Iterate through all elements in codonTable
-for (let i: number = 0; i < rna.length; i++) {
-  const start: number = i * 4
-  codonTable[rna.substring(start, start + 3)] = rna[start + 3]
+const codonTable = {}
+const rnaCodonSplit = rnaCodon.split(/\s+/g)
+for (let i = 0; i < rnaCodonSplit.length; i += 2) {
+  codonTable[rnaCodonSplit[i]] = rnaCodonSplit[i + 1]
 }
-
 // Create array for RNA
 const rnaArray: string[] = []
 
-// Seperate rnaString into individual RNA sequences and transform these sequences into its corresponding amino acid
-for (let i: number = 0; i <= rnaString.length; i+=3) {
-const threeLetters = rnaString.substring(i , i + 3)
-  if (threeLetters in codonTable) {
+// Separate rnaString into individual RNA sequences and transform these sequences into its corresponding amino acid
+for (let i: number = 0; i < rnaString.length; i += 3) {
+  const threeLetters = rnaString.substring(i, i + 3)
+  if (codonTable.hasOwnProperty(threeLetters) && codonTable[threeLetters] !== "Stop") {
     rnaArray.push(codonTable[threeLetters])
   }
 }
